b67ad6927a4979ad84ff1269abf0249378c93de9
Brandon Keao <Brandon.Keao@gmail.com>
2018-06-27 22:36:13 -0600

    added check for existing user in mongodb user collection

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if(existingUser) {
+            // we already have a recode with the given profile Id
+          }
+          else
+            // we don't have a user record with this id, make a new record
+            new User({ googleId: profile.id }).save();
+        })
+
+
     }
   )
 );
